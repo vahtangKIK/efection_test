b126b3d2bb9b906878a73f1e43f595a1e63895d2
Andrey Datsenko <datcenko@softlab.com.ua>
2017-11-30 22:40:11 +0200

    initial commit with database config

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TestDatcenko
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.view_specs = false
+      g.template_engine = nil
+      g.test_framework  = nil #:test_unit, fixture: false
+      g.stylesheets = false
+      g.javascripts  = false      
+    end  
+    config.assets.enabled = false   
+  end
+end
